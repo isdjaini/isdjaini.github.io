e0a71c0ac5ed70eddc79ef3b288485a1680be691
isdjaini <ashuu051020@mail.com>
2022-11-07 16:48:38 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// event pada saaat linkdi klik
+
+$('.page-scrol').on('click', function (e){
+
+	// ambil isi hreff
+	var tujuan = $(this).attr('href');
+	// tangkap elemen yang bersangkutan
+	var elemenTujuan = $(tujuan);
+	// pindahkan scrol
+
+
+});
